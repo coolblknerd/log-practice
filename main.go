5d0f9ddee52c4fff372be90f217a1bf53976517d
Reggie Davis <reginald.davis88@gmail.com>
2019-07-11 22:55:25 -0500

    Updated main.go with correct file

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "log"
 
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
+
 func main() {
 	log.Println("hello")
+	log.Fatalln("fatal message")
+	log.Panicln("panic message")
 }
